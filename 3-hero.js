4def1696cb7ab2b743de40d4e354a5d26cec4125
aryarefman <155049856+aryarefman@users.noreply.github.com>
2024-01-23 15:45:18 +0700

    challenge 3/366

---
+++ 3-hero.js
@@ -0,0 +1,30 @@
+// Coding Challange 2024 
+// Day 3/366
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+// function hero(bullets, dragons) {
+//     if (bullets / 2 >= dragons) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// function hero(bullets, dragons) {
+//     return (bullets / 2 >= dragons) ? true : false;
+// }
+
+
+// function hero(bullets, dragons) {
+//     // truthy / falsy
+//     return bullets / 2 >= dragons;
+// }
+
+
+// const hero = (bullets, dragons) => bullets / 2 >= dragons;
+
+
+const hero = (bullets, dragons) => bullets >= dragons * 2;
+
+console.log(hero(10, 5));
